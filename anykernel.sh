41d32c208ebc245139357b888d167a4632f30588
xxR <yudarmdn16@gmail.com>
2026-07-24 09:11:17 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=KernelSU by KernelSU Developers
+kernel.string=Melt Rebase Rebuild By @Yudharn
 do.devicecheck=0
 do.modules=0
 do.systemless=0
